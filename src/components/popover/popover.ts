efa41321eb533375e12c048e5f854cd39424910c
Cristovão Luiz <cristovoxdgm@gmail.com>
2018-04-17 14:57:16 -0300

    imagens

---
+++ src/components/popover/popover.ts
@@ -0,0 +1,33 @@
+import { Component } from '@angular/core';
+import { ViewController } from 'ionic-angular';
+
+/**
+ * Generated class for the PopoverComponent component.
+ *
+ * See https://angular.io/api/core/Component for more info on Angular
+ * Components.
+ */
+@Component({
+  selector: 'popover',
+  templateUrl: 'popover.html'
+})
+export class PopoverComponent {
+
+  items:any;
+
+  text: string;
+
+  constructor(public viewCTRL:ViewController) {
+      
+    this.items=[
+      {item:'Filtrar'},
+      {item:'Sobre'},
+      {item:'Ajuda?'}
+    ]
+  }
+
+  itemClick(item){
+    this.viewCTRL.dismiss();
+  }
+
+}
