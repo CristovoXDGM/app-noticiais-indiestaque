53ebf1139f44bc4258229cf84642363a09a599fc
Cristovão Luiz <cristovoxdgm@gmail.com>
2018-04-25 18:21:33 -0300

    alterações, sem sacom pra escrever

--- src/pages/trailers/trailers.ts
+++ src/pages/trailers/trailers.ts
@@ -8,6 +8,20 @@ import { NavController } from 'ionic-angular';
 })
 export class TrailersPage {
 
+  trailers = [
+    {
+      //Chamar links direto do banco
+      link:'https://www.youtube.com/watch?v=2kPSl2vyu2Y',
+      thumbnail:'./assets/img/oriori.jpg',
+      titulo:'Melhor jogo indie exclusivo de Xbox one'
+    },
+    {
+      link:'https://www.youtube.com/watch?v=D-1n15aIgsE',
+      thumbnail:'./assets/img/Cuphead.png',
+      titulo:'Melhor jogo indie exclusivo de Xbox one'
+    }
+  ]
+
   constructor(public navCtrl: NavController ) {
   }
 
