a99c12e420449abbc4633565d272b0916d512dc9
Cristovão Luiz <cristovoxdgm@gmail.com>
2018-05-12 13:34:00 -0300

    tentando criar funcao de enviar foto ainda

--- src/pages/cadastrar-noticia/cadastrar-noticia.ts
+++ src/pages/cadastrar-noticia/cadastrar-noticia.ts
@@ -1,3 +1,4 @@
+import { FirebaseCredentials } from './../../app/app.firebase.provider';
 
 import { Component } from '@angular/core';
 import { NavController, NavParams, PopoverController, AlertController } from 'ionic-angular';
@@ -5,8 +6,9 @@ import { PopoverComponent } from '../../components/popover/popover';
 import { AngularFirestore,AngularFirestoreCollection } from 'angularfire2/firestore';
 import { Observable } from 'rxjs/Observable';
 import { NoticiasPage } from '../noticias/noticias';
-
+import {storage,initializeApp } from 'firebase';
 import { CadastrarNoticia } from '../../models/cadastrar-noticia/cadastrar-noticia.interface';
+import { Camera, CameraOptions } from '@ionic-native/camera';
 
 
 @Component({
@@ -14,7 +16,8 @@ import { CadastrarNoticia } from '../../models/cadastrar-noticia/cadastrar-notic
   templateUrl: 'cadastrar-noticia.html',
 })
 export class CadastrarNoticiaPage {
-image:any;
+  
+   
 
   cadastrarnoticia = {} as CadastrarNoticia;
   private noticiasCollection:AngularFirestoreCollection<CadastrarNoticia>;
@@ -24,12 +27,12 @@ image:any;
     public navCtrl: NavController,
     public popoverCtrl : PopoverController, 
     public navParams: NavParams,
-
+    private camera:Camera,
     private database:AngularFirestore,
     public alertCtrl:AlertController
   ) 
   {
-    
+     
     this.noticiasCollection = database.collection<CadastrarNoticia>('Noticias');
     this.noticias = this.noticiasCollection.valueChanges();
   }
@@ -58,5 +61,42 @@ image:any;
     });
     cadastrado.present();
   }
+
+ async Sendphoto(){
+  try{
+    const options: CameraOptions = {
+      
+      correctOrientation: true,
+      allowEdit: true,
+      quality: 100,
+      destinationType: this.camera.DestinationType.DATA_URL,
+      encodingType: this.camera.EncodingType.JPEG,
+      mediaType: this.camera.MediaType.PICTURE,
+      sourceType : this.camera.PictureSourceType.PHOTOLIBRARY
+   
+    }
+    
+    
+    const result = await this.camera.getPicture(options) ;
+
+    const image = 'data:image/jpeg;base64,${result}';
+
+    const pictures = storage().ref('pictures');
+
+    pictures.putString(image,'data_url');
+     } 
+  catch(e){
+     let erro =  this.alertCtrl.create({
+        message:'Erro ao enviar imagem',
+        buttons:[{
+          text:'Continuar',
+          handler:data=>{
+            erro.dismiss();
+          }
+        }]
+      });
+      erro.present();
+    }
+ }
 
 }
